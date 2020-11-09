d987fcc6928fb83ab0187de46da3df3719ea6475
Anas Mohamed <amohamed@ualberta.ca>
2020-11-09 15:41:51 -0700

    done adding comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user, class_name: 'User'
+  has_many :comments, class_name: 'Comment'
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, class_name: 'Post'
+  has_many :comments, class_name: 'Comment'
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
 end
